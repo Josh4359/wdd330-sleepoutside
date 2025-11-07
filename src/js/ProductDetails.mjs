4ada31740251df3fa66d9d757634250754b13bf6
Josh4359 <77248236+Josh4359@users.noreply.github.com>
2025-11-07 15:37:52 -0800

    completed w02 indiv activity

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -2,9 +2,9 @@ import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 export default class ProductDetails {
     constructor(productId, dataSource) {
-        this.productId = productId
-        this.product = {}
-        this.dataSource = dataSource
+        this.productId = productId;
+        this.product = {};
+        this.dataSource = dataSource;
     }
     async init() {
         this.product = await this.dataSource.findProductById(this.productId);
@@ -24,7 +24,7 @@ export default class ProductDetails {
     }
 
     renderProductDetails() {
-        const product = this.product
+        const product = this.product;
         document.querySelector('h2').textContent = product.Brand.Name;
         document.querySelector('h3').textContent = product.NameWithoutBrand;
 
