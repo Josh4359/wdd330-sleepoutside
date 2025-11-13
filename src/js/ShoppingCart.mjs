dd2d0a540920ee7125ca1cc63708c4bd56d53a8f
Josh4359 <77248236+Josh4359@users.noreply.github.com>
2025-11-12 17:19:03 -0800

    completed w03 team activity dynamic header and footer

---
+++ src/js/ShoppingCart.mjs
@@ -0,0 +1,32 @@
+import { renderListWithTemplate } from "./utils.mjs";
+
+function shoppingCartTemplate(item) {
+  return `<li class='cart-card divider'>
+    <a href='/product_pages/?product=${item.Id}' class='cart-card__image'>
+        <img
+        src='${item.Image}'
+        alt='${item.Name}'
+        />
+    </a>
+    <a href='/product_pages/?product=${item.Id}'>
+        <h2 class='card__name'>${item.Name}</h2>
+    </a>
+    <p class='cart-card__color'>${item.Colors[0].ColorName}</p>
+    <p class='cart-card__quantity'>qty: 1</p>
+    <p class='cart-card__price'>$${item.FinalPrice}</p>
+    </li>`;
+}
+
+export default class ShoppingCart {
+    constructor(items, listElement) {
+        this.items = items;
+        this.listElement = listElement
+    }
+    async init() {
+        if (this.items == 0) return
+        this.renderCartContents(this.items)
+    }
+    renderCartContents(items) {
+      renderListWithTemplate(shoppingCartTemplate, this.listElement, items, undefined, true)
+    }
+}
